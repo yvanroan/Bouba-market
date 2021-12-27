96eed4b6eeda9cfff77e113bd52263d9717a34f3
Yvan Taguimdje <yvanroan@yahoo.fr>
2021-12-26 17:20:04 -0700

    make it better

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    
+    <!-- the meta below is used to prevent google from complaining about HTTP request made.-->
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
